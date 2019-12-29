a641fcc8532f01d680c0117ae2f0a7f86a4e0d45
Symamadou <contact.mamadousy@gmail.com>
2019-12-29 18:34:08 +0100

    essai activation formulaire

--- js/monScript.js
+++ js/monScript.js
@@ -3,5 +3,30 @@ $(document).ready(function(){
       $("#formulaire1").hide();
       $(".formulaire2").show();
   });
+  var start = document.getElementsByName('envoyer');
 
+    function valider()
+    {
+    if (document.getElementById('prenom') == ""){
+    alert ("Veuillez entrer votre prénom!");
+    return false;
+    }
+
+    if (document.getElementById('nom') == ""){
+     alert ("Veuillez entrer votre nom!");
+     return false;
+    }
+
+    if (document.getElementById('email') == ""){
+    alert ("Veuillez entrer votre e-mail!");
+      return false;
+    }
+
+    if (document.getElementById('text') == ""){
+    alert ("Veuillez composer votre message!");
+       return false;
+    }
+}
+    
+    start[0].addEventListener('click', valider, false);
 });
