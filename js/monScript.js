4f6bf0fc65be26e124d4c84adc054fee233b1d3e
Symamadou <contact.mamadousy@gmail.com>
2019-12-29 23:39:16 +0100

    activation et animation formulaire

--- js/monScript.js
+++ js/monScript.js
@@ -10,32 +10,22 @@ $(document).ready(function(){
     $("#formulaire1").hide();
     $("#formulaire2").hide();
     $("#formulaire3").show();
-    $("#oui").preventDefault();
-  });
-  var start = document.getElementsByName('envoyer');
-
-    function valider()
-    {
-    if (document.getElementById('prenom') == ""){
-    alert ("Veuillez entrer votre prénom!");
-    return false;
-    }
-
-    if (document.getElementById('nom') == ""){
-     alert ("Veuillez entrer votre nom!");
-     return false;
-    }
-
-    if (document.getElementById('email') == ""){
-    alert ("Veuillez entrer votre e-mail!");
-      return false;
-    }
-
-    if (document.getElementById('text') == ""){
-    alert ("Veuillez composer votre message!");
-       return false;
-    }
-}
     
-    start[0].addEventListener('click', valider, false);
+  });
+
+  $("#oui-footer").click(function(){
+
+    $("#formulaire-footer1").hide();
+    $("#formulaire-footer2").show();
+    $("#oui-footer").preventDefault();
+  });
+
+  $('.js_goform3').click(function(){
+    $("#formulaire-footer1").hide();
+    $("#formulaire-footer2").hide();
+    $("#formulaire-footer3").show();
+    
+  });
+  
+
 });
